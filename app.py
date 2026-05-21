34b0a9754c0c75b639a891447a1631cb326af524
ouchuartz <ouchukhamic39@gmail.com>
2026-05-21 21:23:34 +0300

    Fix verification block placement and activate dynamic environment port mapping

--- app.py
+++ app.py
@@ -516,6 +516,5 @@ def delete_message(record_id):
     return redirect(url_for('admin'))
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
